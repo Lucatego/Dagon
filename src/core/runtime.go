13974502e96fb8a828e67e919fb10f5d78c093bc
Lucatego <lucasalvites@gmail.com>
2025-07-25 15:19:13 -0500

    Updating the runtime

--- src/core/runtime.go
+++ src/core/runtime.go
@@ -1,8 +1,36 @@
 package core
 
+import (
+	"os"
+	"os/user"
+)
+
 /*
 	TODO: Get the dir of work, get the username, get the hostname.
 */
-func StartRuntime() {
 
+var (
+	Hostname string
+	Username string
+	Dir      string
+)
+
+func StartRuntime() {
+	// Get the hostname
+	host, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	Hostname = host
+	// Get the Username
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	Username = user.Username
+	// Get the current working directory
+	Dir, err = os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 }
